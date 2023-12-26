4be9338f2183c8a4bb5cb55a09fe2d50a6b7bedd
wkdehf217 <wkdehfdl17@naver.com>
2023-12-26 16:00:28 +0900

    global init

---
+++ src/main/java/com/bts/trelloproject/global/common/BaseCreatedTimeEntity.java
@@ -0,0 +1,19 @@
+package com.bts.trelloproject.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class BaseCreatedTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdTime;
+}
